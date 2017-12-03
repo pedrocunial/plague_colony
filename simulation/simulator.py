669156b25704445bfdebf0a74ecb85e709b0302c
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2017-12-03 12:17:43 -0200

    Add function to calculate win probability

--- simulation/simulator.py
+++ simulation/simulator.py
@@ -12,7 +12,9 @@ class Simulator:
     def __init__(self, g_obj):
         self.g = g_obj.g
         self.g_obj = g_obj
-
+        self.win_eq = [
+            (1, 0.7)
+        ]
 
     def _count_pt_neighbors(self, n):
         return sum(list(map(lambda x: x[1],
@@ -44,12 +46,30 @@ class Simulator:
         return self.g.node[n]['nationality'] == self.PORTUGUESE and \
             self.g.node[m]['nationality'] == self.NATIVE
 
+    def _calculate_win_probability(self, n, m):
+        '''
+        calculates win probability based on equation
+        P = k0*F0 + k1*F1 + ... + kn*Fn
+            ---------------------------
+                       sum(k)
+        '''
+        prob = 0
+
+        pop_n = self.g.node[n]['population'] / \
+            (self.g.node[n]['population'] + self.g.node[m]['population'])
+
+        self.win_eq.append((0.5, pop_n))
+
+        for term in self.win_eq:
+            prob += term[0] * term[1]
+
+        return prob / sum([x[1] for x in self.win_eq])
 
     def _fight(self, n, m):
         '''
         returns the looser of a simulated fight between n and m
         '''
-        P = 70  # 70% chance
+        P = self._calculate_win_probability(n, m)
         if self.g.node[n]['nationality'] == self.g.node[m]['nationality'] and \
            self.g.node[n]['nationality'] == self.NATIVE:
             return self._fight_native_vs_native(n, m)
